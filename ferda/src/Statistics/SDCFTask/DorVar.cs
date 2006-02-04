9fd57b2c94735bf748d399cb39872ddfc369f1f5
kuzmos <kuzmos@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2006-02-04 13:46:52 +0000

    - rest of statistics skeleton

---
+++ ferda/src/Statistics/SDCFTask/DorVar.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferda.Statistics.SDCFTask
+{
+    class DorVar : Ferda.Statistics.StatisticsProviderDisp_
+    {
+        public override float getStatistics(Ferda.Modules.AbstractQuantifierSetting quantifierSetting, Ice.Current current__)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override string getTaskType(Ice.Current current__)
+        {
+            return "SDCFTask";
+        }
+
+        public override string getStatisticsName(Ice.Current current__)
+        {
+            return "Sum";
+        }
+    }
+}
