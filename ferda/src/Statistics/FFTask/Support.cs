88d1b6f5a860a4eed697b54587fe140f7f6341c2
kuzmos <alexander.kuzmin@gmail.com>
2006-02-04 13:22:36 +0000

    - creating skeleton for statistics, backup commit

---
+++ ferda/src/Statistics/FFTask/Support.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferda.Statistics.FFT
+{
+    class Support : Ferda.Statistics.StatisticsProviderDisp_
+    {
+        public override float getStatistics(Ferda.Modules.AbstractQuantifierSetting quantifierSetting, Ice.Current current__)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override string getTaskType(Ice.Current current__)
+        {
+            return "FFTask";
+        }
+
+        public override string getStatisticsName(Ice.Current current__)
+        {
+            return "Support";
+        }
+    }
+}
---
+++ ferda/src/Statistics/SDFFT/DLBound.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferda.Statistics.SDFFT
+{
+    class DLBound : Ferda.Statistics.StatisticsProviderDisp_
+    {
+        public override float getStatistics(Ferda.Modules.AbstractQuantifierSetting quantifierSetting, Ice.Current current__)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override string getTaskType(Ice.Current current__)
+        {
+            return "SDFFTask";
+        }
+
+        public override string getStatisticsName(Ice.Current current__)
+        {
+            return "DLBound";
+        }
+    }
+}
