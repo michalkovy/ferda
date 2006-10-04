31844f50f69eeb6067f643d73a4f52175e1a2436
kanec_martin <kanec_martin@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2006-10-04 10:44:40 +0000

    The MFI ExplainTable should be functional for the new version

--- ferda/src/FrontEnd/AddIns/ExplainTable/AssemblyInfo.cs
+++ ferda/src/FrontEnd/AddIns/ExplainTable/AssemblyInfo.cs
@@ -1,26 +1,26 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle(" Ferda FrontEnd")]
-[assembly: AssemblyDescription("User interface for data mining tool Ferda")]
+[assembly: AssemblyTitle("Explain Table")]
+[assembly: AssemblyDescription("Module for interaction gives description about a data table")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kanci")]
-[assembly: AssemblyProduct("FrontEnd")]
-[assembly: AssemblyCopyright("Copyright  Martin Ralbovsky Ferda")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("Ferda DataMiner")]
+[assembly: AssemblyCopyright("Ferda")]
+[assembly: AssemblyTrademark("Ferda")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM componenets.  If you need to access a type in this assembly from 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c3e277f-4f98-498f-9b91-5de25fcfa350")]
+[assembly: Guid("f54aa2ec-4bb0-4555-8103-e7ca2a9d1af4")]
 
 // Version information for an assembly consists of the following four values:
 //
