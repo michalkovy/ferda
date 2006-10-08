7b9d5e80f1c5e79448f687754c574f69137da81c
kuzmos <kuzmos@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2006-10-08 13:13:52 +0000

    - added equifrequency intervals box

--- ferda/src/Modules/BoxModulesServices/DataPreparation/Categorization/EquifrequencyIntervals/BoxInfo.cs
+++ ferda/src/Modules/BoxModulesServices/DataPreparation/Categorization/EquifrequencyIntervals/BoxInfo.cs
@@ -1,6 +1,67 @@
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Ferda.Guha.Data;
+using Object = Ice.Object;
+
 namespace Ferda.Modules.Boxes.DataPreparation.Categorization.EquifrequencyIntervals
 {
-    internal class BoxInfo
+    internal class BoxInfo : Boxes.BoxInfo
     {
+        public override void CreateFunctions(BoxModuleI boxModule, out Object iceObject, out IFunctions functions)
+        {
+            Functions result = new Functions();
+            iceObject = result;
+            functions = result;
+        }
+
+        public override string[] GetBoxModuleFunctionsIceIds()
+        {
+            return Functions.ids__;
+        }
+
+        public override string GetDefaultUserLabel(BoxModuleI boxModule)
+        {
+            return ((Functions)boxModule.FunctionsIObj).NameInLiterals;
+        }
+
+        public override ModulesAskingForCreation[] GetModulesAskingForCreation(string[] localePrefs, BoxModuleI boxModule)
+        {
+            return new ModulesAskingForCreation[0];
+        }
+
+        public override PropertyValue GetReadOnlyPropertyValue(string propertyName, BoxModuleI boxModule)
+        {
+            Functions Func = (Functions)boxModule.FunctionsIObj;
+            switch (propertyName)
+            {
+                case Functions.PropIncludeNullCategory:
+                    return Func.IncludeNullCategory;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public override SelectString[] GetPropertyOptions(string propertyName, BoxModuleI boxModule)
+        {
+            Functions Func = (Functions)boxModule.FunctionsIObj;
+
+            switch (propertyName)
+            {
+                case Functions.PropXCategory:
+                    return BoxInfoHelper.GetSelectStringArray(
+                        Func.GetCategoriesNames(false)
+                        );
+                default:
+                    return null;
+            }
+        }
+
+        public const string typeIdentifier = "DataPreparation.Categorization.EquifrequencyIntervals";
+
+        protected override string identifier
+        {
+            get { return typeIdentifier; }
+        }
     }
 }
