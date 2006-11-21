6179dfa82d0cb01e389f5e5e744e1dba2e5b8dfa
kanec_martin <martin.ralbovsky@gmail.com>
2006-11-21 11:25:33 +0000

    Added some documentation into the DataSource box and better context help component of the Front End

--- ferda/slice/Modules/Boxes/DataPreparation/DataPreparation.ice
+++ ferda/slice/Modules/Boxes/DataPreparation/DataPreparation.ice
@@ -1,3 +1,25 @@
+/*
+Functions.cs - Function objects for the database box module
+
+Author: Tomáš Kuchaø <tomas.kuchar@gmail.com>
+Documented by: Martin Ralbovský <martin.ralbovsky@gmail.com>
+
+Copyright (c) 2006 Tomáš Kuchaø, Martin Ralbovský
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 #ifndef	FERDA_MODULES_BOXES_DATAPREPARATION
 #define	FERDA_MODULES_BOXES_DATAPREPARATION
 
@@ -15,12 +37,16 @@ module Ferda {
 					// DATABASE
 					
 					interface DatabaseFunctions {
+					     //Returns the connection string
 						nonmutating	Ferda::Guha::Data::DatabaseConnectionSetting	getDatabaseConnectionSetting()
 							throws Ferda::Modules::BoxRuntimeError;
+						//Returns the connection info
 						nonmutating	Ferda::Guha::Data::ConnectionInfo getConnectionInfo()
 							throws Ferda::Modules::BoxRuntimeError;
+						//Returns an array of structures describing the tables in the database
 						nonmutating Ferda::Guha::Data::DataTableExplainSeq getDataTableExplainSeq()
 							throws Ferda::Modules::BoxRuntimeError;
+						//Returns the names of tables in the database
 						nonmutating StringSeq getDataTablesNames()
 							throws Ferda::Modules::BoxRuntimeError;
 					};
--- ferda/src/Modules/BoxModulesServices/DataPreparation/DataSource/Database/Functions.cs
+++ ferda/src/Modules/BoxModulesServices/DataPreparation/DataSource/Database/Functions.cs
@@ -1,3 +1,24 @@
+// Functions.cs - Function objects for the database box module
+//
+// Author: Tomáš Kuchaø <tomas.kuchar@gmail.com>
+// Documented by: Martin Ralbovský <martin.ralbovsky@gmail.com>
+//
+// Copyright (c) 2006 Tomáš Kuchaø, Martin Ralbovský
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -7,14 +28,16 @@ using Ice;
 
 namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
 {
+    /// <summary>
+    /// Class is providing ICE functionality of the Database
+    /// box module
+    /// </summary>
     internal class Functions : DatabaseFunctionsDisp_, IFunctions
     {
         /// <summary>
-        /// The box module.
+        /// The current
         /// </summary>
         protected BoxModuleI _boxModule;
-
-        //protected IBoxInfo _boxInfo;
 
         #region IFunctions Members
 
@@ -34,6 +57,7 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
 
         #region Properties
 
+        //names of the properties
         public const string PropProviderInvariantName = "ProviderInvariantName";
         public const string PropConnectionString = "ConnectionString";
         public const string PropLastReloadRequest = "LastReloadRequest";
@@ -44,11 +68,17 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
         public const string PropDriver = "Driver";
         public const string PropServerVersion = "ServerVersion";
 
+        /// <summary>
+        /// The Provider invariant name property
+        /// </summary>
         public string ProviderInvariantName
         {
             get { return _boxModule.GetPropertyString(PropProviderInvariantName); }
         }
 
+        /// <summary>
+        /// The Connection string property
+        /// </summary>
         public string ConnectionString
         {
             get { return _boxModule.GetPropertyString(PropConnectionString); }
@@ -56,6 +86,9 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
 
         private DateTimeTI _lastReloadRequest = null;
 
+        /// <summary>
+        /// The Last Reload Request property
+        /// </summary>
         public DateTimeTI LastReloadRequest
         {
             get
@@ -71,11 +104,17 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
             }
         }
 
+        /// <summary>
+        /// The Acceptable Data Types property
+        /// </summary>
         public string[] AcceptableDataTableTypes
         {
             get { return _boxModule.GetPropertyStringSeq(PropAcceptableDataTableTypes); }
         }
 
+        /// <summary>
+        /// The Connection timeout property
+        /// </summary>
         public IntTI ConnectionTimeout
         {
             get
@@ -85,6 +124,9 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
             }
         }
 
+        /// <summary>
+        /// The Database name property
+        /// </summary>
         public StringTI DatabaseName
         {
             get
@@ -94,6 +136,9 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
             }
         }
 
+        /// <summary>
+        /// The Data source property
+        /// </summary>
         public StringTI DataSource
         {
             get
@@ -103,6 +148,9 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
             }
         }
 
+        /// <summary>
+        /// The Driver property
+        /// </summary>
         public StringTI Driver
         {
             get
@@ -112,6 +160,9 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
             }
         }
 
+        /// <summary>
+        /// The Server version property
+        /// </summary>
         public StringTI ServerVersion
         {
             get
@@ -128,6 +179,11 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
         private CacheFlag _cacheFlag = new CacheFlag();
         private GenericDatabase _cachedValue = null;
 
+        /// <summary>
+        /// Gets the generic database with aid of cashing
+        /// </summary>
+        /// <param name="fallOnError">If to fall on error</param>
+        /// <returns>The generic database</returns>
         public GenericDatabase GetGenericDatabase(bool fallOnError)
         {
             DatabaseConnectionSettingHelper connSetting =
@@ -155,6 +211,12 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
             return _cachedValue;
         }
 
+        /// <summary>
+        /// Function returns the <see cref="T:Ferda.Guha.Data.ConnectionInfo">
+        /// Connection Info</see> structure
+        /// </summary>
+        /// <param name="fallOnError">If to fall on error</param>
+        /// <returns>The connection info</returns>
         public ConnectionInfo GetConnectionInfo(bool fallOnError)
         {
             return ExceptionsHandler.GetResult<ConnectionInfo>(
@@ -174,6 +236,12 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
                 );
         }
 
+        /// <summary>
+        /// Gets the array of<see cref="T:Ferda.Guha.Data.DataTableExplain">DataTableExplain
+        /// </see> structures that hold information about the tables in the database
+        /// </summary>
+        /// <param name="fallOnError">If to fall on error</param>
+        /// <returns>Information about data tables</returns>
         public DataTableExplain[] GetDataTableExplainSeq(bool fallOnError)
         {
             string[] accepts = AcceptableDataTableTypes;
@@ -203,6 +271,11 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
                 );
         }
 
+        /// <summary>
+        /// Gets the names of data tables in the database
+        /// </summary>
+        /// <param name="fallOnError">If to fail on error</param>
+        /// <returns>The names of data tables in the database</returns>
         public string[] GetDataTablesNames(bool fallOnError)
         {
             return ExceptionsHandler.GetResult<string[]>(
@@ -226,21 +299,43 @@ namespace Ferda.Modules.Boxes.DataPreparation.Datasource.Database
 
         #region Ice Functions
 
+        /// <summary>
+        /// Gets the <see cref="T:Ferda.Guha.Data.DatabaseConnectionSetting">Connection
+        /// setting</see> object of the database
+        /// </summary>
+        /// <param name="current__">Ice stuff</param>
+        /// <returns>The database connection setting</returns>
         public override DatabaseConnectionSetting getDatabaseConnectionSetting(Current current__)
         {
             return new DatabaseConnectionSetting(ProviderInvariantName, ConnectionString, LastReloadRequest);
         }
 
+        /// <summary>
+        /// Gets the <see cref="T:Ferda.Guha.Data.ConnectionInfo">ConnectionInfo</see>
+        /// structure that holds information about the connection
+        /// </summary>
+        /// <param name="current__">Ice stuff</param>
+        /// <returns>Connection info</returns>
         public override ConnectionInfo getConnectionInfo(Current current__)
         {
             return GetConnectionInfo(true);
         }
 
+        /// <summary>
+        /// Gets information about tables in the database
+        /// </summary>
+        /// <param name="current__">Ice stuff</param>
+        /// <returns>Information about the tables in the database</returns>
         public override DataTableExplain[] getDataTableExplainSeq(Current current__)
         {
             return GetDataTableExplainSeq(true);
         }
 
+        /// <summary>
+        /// Gets names of tables in the database
+        /// </summary>
+        /// <param name="current__">Ice stuff</param>
+        /// <returns>Names of tables</returns>
         public override string[] getDataTablesNames(Current current__)
         {
             return GetDataTablesNames(true);
