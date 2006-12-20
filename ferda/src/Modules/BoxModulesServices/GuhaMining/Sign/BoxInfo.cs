0822930047a4147220b57a818cc3bdf644bb2254
kanec_martin <kanec_martin@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2006-12-20 12:07:34 +0000

    Modules asking for creation implemented for the ConjunctionSetting, DisjunctionSetting and Sign boxes

--- ferda/src/Modules/BoxModulesServices/GuhaMining/Sign/BoxInfo.cs
+++ ferda/src/Modules/BoxModulesServices/GuhaMining/Sign/BoxInfo.cs
@@ -1,6 +1,29 @@
+// BoxInfo.cs - BoxInfo class for the sign box
+//
+// Authors: Tomáš Kuchaø <tomas.kuchar@gmail.com>,
+//          Martin Ralbovský <martin.ralbovsky@gmail.com> (modulesAFC)
+//
+// Copyright (c) 2006 Tomáš Kuchaø, Martin Ralbovský
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 using Ferda.Guha.MiningProcessor;
 using Ferda.Guha.MiningProcessor.Formulas;
-using Ice;
+using Object = Ice.Object;
+using System;
+using System.Collections.Generic;
 
 namespace Ferda.Modules.Boxes.GuhaMining.Sign
 {
@@ -32,10 +55,87 @@ namespace Ferda.Modules.Boxes.GuhaMining.Sign
                 return FormulaHelper.NegationSign + inputBoxLabel;
         }
 
+        /// <summary>
+        /// Gets the box modules asking for creation.
+        /// </summary>
+        /// <param name="localePrefs">The localization preferences.</param>
+        /// <param name="boxModule">The box module.</param>
+        /// <returns>
+        /// Array of <see cref="T:Ferda.Modules.ModuleAskingForCreation">
+        /// Modules Asking For Creation</see>.
+        /// </returns>
         public override ModulesAskingForCreation[] GetModulesAskingForCreation(string[] localePrefs,
                                                                                BoxModuleI boxModule)
         {
-            return new ModulesAskingForCreation[0];
+            //getting the information what is in the config files
+            Dictionary<string, ModulesAskingForCreation> modulesAFC =
+                getModulesAskingForCreationNonDynamic(localePrefs);
+            //creating the structure that will be returned
+            List<ModulesAskingForCreation> result =
+                new List<ModulesAskingForCreation>();
+
+            ModulesConnection moduleConnection;
+            ModuleAskingForCreation singleModule;
+
+            foreach (string moduleAFCname in modulesAFC.Keys)
+            {
+                singleModule = new ModuleAskingForCreation();
+                moduleConnection = new ModulesConnection();
+                //no need to set any property
+                singleModule.propertySetting = new PropertySetting[] { };
+
+                switch (moduleAFCname)
+                {
+                    case "ConjunctionSetting":
+                        //creating the info about the connections of the new module
+                        moduleConnection.socketName =
+                            ConjunctionSetting.Functions.SockBooleanAttributeSetting;
+                        moduleConnection.boxModuleParam = boxModule.MyProxy;
+
+                        //creating the new (single) module
+                        singleModule.modulesConnection =
+                            new ModulesConnection[] { moduleConnection };
+                        singleModule.newBoxModuleIdentifier =
+                            ConjunctionSetting.BoxInfo.typeIdentifier;
+                        break;
+
+                    case "DisjunctionSetting":
+                        //creating the info about the connections of the new module
+                        moduleConnection.socketName =
+                            DisjunctionSetting.Functions.SockBooleanAttributeSetting;
+                        moduleConnection.boxModuleParam = boxModule.MyProxy;
+
+                        //creating the new (single) module
+                        singleModule.modulesConnection =
+                            new ModulesConnection[] { moduleConnection };
+                        singleModule.newBoxModuleIdentifier =
+                            DisjunctionSetting.BoxInfo.typeIdentifier;
+                        break;
+
+                    case "ClassOfEquivalence":
+                        //creating the info about the connections of the new module
+                        moduleConnection.socketName =
+                            ClassOfEquivalence.Functions.SockBooleanAttributeSetting;
+                        moduleConnection.boxModuleParam = boxModule.MyProxy;
+
+                        //creating the new (single) module
+                        singleModule.modulesConnection =
+                            new ModulesConnection[] { moduleConnection };
+                        singleModule.newBoxModuleIdentifier =
+                            ClassOfEquivalence.BoxInfo.typeIdentifier;
+                        break;
+
+                    default:
+                        throw new NotImplementedException();
+                }
+
+                //setting the newModules property of each modules for intearction
+                modulesAFC[moduleAFCname].newModules =
+                    new ModuleAskingForCreation[] { singleModule };
+                result.Add(modulesAFC[moduleAFCname]);
+            }
+
+            return result.ToArray();
         }
 
         public override SelectString[] GetPropertyOptions(string propertyName, BoxModuleI boxModule)
