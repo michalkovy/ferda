ae9746f75df39bc6e0efee1a145b727169633e2b
kippo <kippo@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2007-03-02 19:14:44 +0000

    with string

---
+++ ferda/src/FrontEnd/AddIns/RegEditor/MyIce/RegEditorIce.cs
@@ -0,0 +1,239 @@
+// SelectTablesIce.cs - class for ice communication
+//
+// Author: Daniel KUpka <kupkd9am@post.cz>
+//
+// Copyright (c) 2007 Daniel Kupka
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ferda.Modules;
+using Ferda.Modules.Boxes;
+using Ferda.ModulesManager;
+using Ferda.FrontEnd.AddIns;
+using System.Resources;
+using System.Reflection;
+using PropertyInfo=Ferda.Modules.PropertyInfo;
+using System.Windows.Forms;
+
+namespace Ferda.FrontEnd.AddIns.RegEditor.MyIce
+{
+    /// <summary>
+    /// Class for ice communication
+    /// </summary>
+    class RegEditorIce : SettingModuleDisp_
+    {
+        #region Private variables
+
+        /// <summary>
+        /// Owner of addin
+        /// </summary>
+        Ferda.FrontEnd.AddIns.IOwnerOfAddIn ownerOfAddIn;
+
+        /// <summary>
+        /// L10n resource manager
+        /// </summary>
+        private ResourceManager resManager;
+
+        /// <summary>
+        /// L10n string, for now en-US or cs-CZ
+        /// </summary>
+        private string localizationString;
+
+        /// <summary>
+        /// Resulting string with selected tables
+        /// </summary>
+        string [] returnStrings;
+
+        /// <summary>
+        /// Resulting string with XML content
+        /// </summary>
+        string returnString;
+
+
+        #endregion
+
+
+        #region Constructor
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="ownerOfAddIn">Owner of addin</param>
+        public RegEditorIce(Ferda.FrontEnd.AddIns.IOwnerOfAddIn ownerOfAddIn)
+        {
+            this.ownerOfAddIn = ownerOfAddIn;
+
+            //setting the ResManager resource manager and localization string
+            //resManager = new ResourceManager("Ferda.FrontEnd.AddIns.MultiSelectStrings.Localization_en-US",
+            //Assembly.GetExecutingAssembly());
+            //localizationString = "en-US";
+        }
+
+        #endregion
+
+
+        #region Other ice
+
+
+        public override string getIdentifier(Ice.Current current__)
+        {
+            return "RegEditor";
+        }
+
+        public override string getLabel(string[] localePrefs, Ice.Current current__)
+        {
+            string locale;
+            /*try
+            {
+               locale = localePrefs[0];
+                localizationString = locale;
+                locale = "Ferda.FrontEnd.AddIns.MultiSelectStrings.Localization_" + locale;
+                resManager = new ResourceManager(locale, Assembly.GetExecutingAssembly());
+            }
+            catch { }
+            return resManager.GetString("FormEditor");*/
+            return "";
+        }
+
+        public override string getPropertyAbout(PropertyValue value, Ice.Current current__)
+        {
+            return "";
+        }
+
+        #endregion
+
+
+        #region Icerun
+
+        /// <summary>
+        /// Ice run
+        /// </summary>
+        /// <param name="valueBefore">Previous value</param>
+        /// <param name="boxModuleParam">boxmoduleparam</param>
+        /// <param name="localePrefs">localeprefs</param>
+        /// <param name="manager">Manager proxy</param>
+        /// <param name="about"></param>
+        /// <param name="current__">Ice context</param>
+        /// <returns>Modified property value</returns>
+        public override PropertyValue run(PropertyValue valueBefore, string propertyName, BoxModulePrx boxModuleParam, string[] localePrefs, ManagersEnginePrx manager, out string about, Ice.Current current__)
+        {
+            string locale;
+
+            StringT XMLString = (StringT)valueBefore;
+            PropertyValue returnValue = new PropertyValue();
+            PropertyValue propertyValue = valueBefore;
+
+
+            RegEditor.RegularExpEditor control =
+                new RegEditor.RegularExpEditor(XMLString.getStringValue());
+
+            control.Disposed += new EventHandler(control_Disposed);
+            System.Windows.Forms.DialogResult result = this.ownerOfAddIn.ShowDialog(control);
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                XMLString.stringValue = this.returnString;
+                PropertyValue resultValue = XMLString;
+                // about = this.getPropertyAbout(resultValue);
+                about = "Reg editor about";
+                propertyValue = resultValue;
+            }
+            else
+            {
+                //about = this.getPropertyAbout(valueBefore);
+                about = "reg";
+                return valueBefore;
+            }
+
+            //PropertyValue propertyValue = null;
+
+
+
+            //propertyValue = result;
+            /* try
+             {
+                 locale = localePrefs[0];
+                 localizationString = locale;
+                 locale = "Ferda.FrontEnd.AddIns.MultiSelectStrings.Localization_" + locale;
+                 resManager = new ResourceManager(locale, Assembly.GetExecutingAssembly());
+             }
+             catch {}*/
+
+            /*   StringSeqT selectedStrings = (StringSeqT)valueBefore;
+               PropertyValue propertyValue = valueBefore;
+               PropertyValue returnValue = new PropertyValue();
+
+               // for dynamicaly changing select box params
+               SelectString[] selStrings = boxModuleParam.getPropertyOptions(propertyName);
+            
+               // for static select box params
+               if (selStrings == null || selStrings.Length == 0)
+               {
+                   PropertyInfo[] properties = boxModuleParam.getMyFactory().getProperties();
+                   foreach (PropertyInfo property in properties)
+                   {
+                       if (property.name == propertyName)
+                           selStrings = property.selectBoxParams;
+                   }
+               }
+            
+               MultiSelectStrings.MultiSelectStringsControl listView =
+                   new MultiSelectStrings.MultiSelectStringsControl(localePrefs,
+                   ownerOfAddIn,
+                   selStrings,
+                   selectedStrings.stringSeqValue
+                   );
+
+               listView.ShowInTaskbar = false;
+               listView.Disposed += new EventHandler(listView_Disposed);
+               System.Windows.Forms.DialogResult result = this.ownerOfAddIn.ShowDialog(listView);
+               if (result == System.Windows.Forms.DialogResult.OK)
+               {
+                   selectedStrings.stringSeqValue = this.returnStrings;
+                   PropertyValue resultValue = selectedStrings;
+                   about = this.getPropertyAbout(resultValue);
+                   propertyValue = resultValue;
+               }
+               else
+               {
+                   about = this.getPropertyAbout(valueBefore);
+                   return valueBefore;
+               }*/
+            return propertyValue;
+        }
+
+        #endregion
+
+
+        #region Other private methods
+
+        /// <summary>
+        /// Handler of disposing event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void control_Disposed(object sender, EventArgs e)
+        {
+            Ferda.FrontEnd.AddIns.RegEditor.RegularExpEditor control =
+                (Ferda.FrontEnd.AddIns.RegEditor.RegularExpEditor)sender;
+
+            this.returnString = control.ReturnString;
+        }
+
+        #endregion
+    }
+}
