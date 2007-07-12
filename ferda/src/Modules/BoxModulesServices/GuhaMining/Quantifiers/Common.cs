fcf2862484dca0abf4d8e54e36eecabcb1621373
kanec_martin <kanec_martin@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2007-07-12 12:10:55 +0000

    Comments added

--- ferda/src/Modules/BoxModulesServices/GuhaMining/Quantifiers/Common.cs
+++ ferda/src/Modules/BoxModulesServices/GuhaMining/Quantifiers/Common.cs
@@ -1,3 +1,25 @@
+// Common.cs - Common functionality for GUHA quantifiers implemented
+// in the Ferda system
+//
+// Author: Tomáš Kuchaø <tomas.kuchar@gmail.com>
+// Documented by: Martin Ralbovský <martin.ralbovsky@gmail.com>
+//
+// Copyright (c) 2006 Tomáš Kuchaø, Martin Ralbovský
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 using System;
 using System.Collections.Generic;
 using Ferda.Guha.Math.Quantifiers;
@@ -6,10 +28,15 @@ using Ferda.Modules.Helpers.Caching;
 
 namespace Ferda.Modules.Boxes.GuhaMining.Quantifiers
 {
+    /// <summary>
+    /// Class providing static functions that are in common to all
+    /// GUHA quantifiers
+    /// </summary>
     public static class Common
     {
         #region Properties
 
+        //names of the properties/sockets
         public const string PropOperationMode = "OperationMode";
         public const string PropMissingInformationHandling = "MissingInformationHandling";
         public const string PropRelation = "Relation";
@@ -35,8 +62,18 @@ namespace Ferda.Modules.Boxes.GuhaMining.Quantifiers
 
         #endregion
 
+        /// <summary>
+        /// Cache of numeric values of quantifiers computation
+        /// </summary>
         private static NumericValuesCache _numericValuesCache = new NumericValuesCache();
 
+        /// <summary>
+        /// Retrieves values numerical values of a (probably one dimensional)
+        /// contingency table. The function is used when computing distribution
+        /// characteristics of a cardinal variable.
+        /// </summary>
+        /// <param name="param">Settings for quantifier evaluation</param>
+        /// <returns>Numerical values of a (one dimensional) contingency table</returns>
         public static double[] GetNumericValues(QuantifierEvaluateSetting param)
         {
             Guid numericValuesAttributeId = new Guid(param.numericValuesAttributeId.value);
@@ -49,8 +86,21 @@ namespace Ferda.Modules.Boxes.GuhaMining.Quantifiers
                 ];
         }
         
+        /// <summary>
+        /// Delegate for computation of a quantifier over
+        /// <see cref="Ferda.Guha.Math.Quantifiers.QuantifierEvaluateSetting"/>.
+        /// </summary>
+        /// <param name="param">Setting for quantifier evaluation</param>
+        /// <returns>Iff the quantifier is valid over the setting</returns>
         public delegate bool ComputeDelegate(QuantifierEvaluateSetting param);
         
+        /// <summary>
+        /// Executes a batch computation of quantifiers.
+        /// </summary>
+        /// <param name="param">Settings for quantifier evaluation</param>
+        /// <param name="compute">Computation delegate</param>
+        /// <returns>Array of boolean values corresponding to the validity
+        /// of quantifier evaluation settings in <paramref name="param"/></returns>
         public static bool[] ComputeBatch(QuantifierEvaluateSetting[] param, ComputeDelegate compute)
         {
             List<bool> result = new List<bool>(param.Length);
@@ -62,11 +112,27 @@ namespace Ferda.Modules.Boxes.GuhaMining.Quantifiers
         }
     }
 
+    /// <summary>
+    /// Class for evaluating two equality of two pairs of 
+    /// objects identified by their GUID
+    /// </summary>
+    /// <typeparam name="T">Type of any kind</typeparam>
     public class GuidPrxPair<T> : IEquatable<GuidPrxPair<T>>
     {
+        /// <summary>
+        /// Identifier of the object
+        /// </summary>
         public Guid Guid;
+        /// <summary>
+        /// Object itself
+        /// </summary>
         public T Prx;
 
+        /// <summary>
+        /// Default construcor for the class
+        /// </summary>
+        /// <param name="guid">Identifier of an object</param>
+        /// <param name="prx">Object</param>
         public GuidPrxPair(Guid guid, T prx)
         {
             Guid = guid;
@@ -75,6 +141,11 @@ namespace Ferda.Modules.Boxes.GuhaMining.Quantifiers
 
         #region IEquatable<GuidPrxPair<T>> Members
 
+        /// <summary>
+        /// Function returns equality of objects
+        /// </summary>
+        /// <param name="other">The compared object</param>
+        /// <returns>Equality</returns>
         public bool Equals(GuidPrxPair<T> other)
         {
             if (other == null)
@@ -87,15 +158,30 @@ namespace Ferda.Modules.Boxes.GuhaMining.Quantifiers
         #endregion
     }
 
+    /// <summary>
+    /// Cache for numeric values of an attribute. It is a dictionary indexed by the
+    /// bit string generator identification.
+    /// </summary>
     public class NumericValuesCache : MostRecentlyUsed<GuidPrxPair<BitStringGeneratorPrx>, double[]>
     {
+        /// <summary>
+        /// Default size of the cache
+        /// </summary>
         public const int DefaultNumericValuesCacheSize = 1;
 
+        /// <summary>
+        /// Default constructor for the class
+        /// </summary>
         public NumericValuesCache()
             : base(DefaultNumericValuesCacheSize)
         {
         }
 
+        /// <summary>
+        /// Gets value indexed by the bit string generator identification
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <returns>Value (numerical values)</returns>
         public override double[] GetValue(GuidPrxPair<BitStringGeneratorPrx> key)
         {
             return key.Prx.GetCategoriesNumericValues();
--- ferda/src/Modules/BoxModulesServices/GuhaMining/Tasks/SDSingleDimensional/BoxInfo.cs
+++ ferda/src/Modules/BoxModulesServices/GuhaMining/Tasks/SDSingleDimensional/BoxInfo.cs
@@ -1,10 +1,41 @@
+// BoxInfo.cs - BoxInfo class for the SDCF task box
+//
+// Author: Tomáš Kuchaø <tomas.kuchar@gmail.com>,
+// Documented by: Martin Ralbovský <martin.ralbovsky@gmail.com>
+//
+// Copyright (c) 2006 Tomáš Kuchaø, Martin Ralbovský
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 using System;
 using Object=Ice.Object;
 
 namespace Ferda.Modules.Boxes.GuhaMining.Tasks.SDSingleDimensional
 {
+    /// <summary>
+    /// Class that provides info about boxes of the SDCF task type
+    /// </summary>
     internal class BoxInfo : Boxes.BoxInfo
     {
+        /// <summary>
+        /// Functions creates an object of <see cref="T:Ferda.Modules.IFunctions">IFuntions</see>
+        /// type that provides functionality of the box
+        /// </summary>
+        /// <param name="boxModule">Current box module</param>
+        /// <param name="iceObject">ICE stuff</param>
+        /// <param name="functions">The new created functions object</param>
         public override void CreateFunctions(BoxModuleI boxModule, out Object iceObject, out IFunctions functions)
         {
             Functions result = new Functions();
@@ -12,34 +43,68 @@ namespace Ferda.Modules.Boxes.GuhaMining.Tasks.SDSingleDimensional
             functions = result;
         }
 
+        /// <summary>
+        /// Gets function`s Ice identifiers of the box module.
+        /// </summary>
+        /// <returns>
+        /// An array of strings representing Ice identifiers
+        /// of the box module`s functions.
+        /// </returns>
+        /// <example>
+        /// Please see an example for <see cref="T:Ferda.Modules.Boxes.IBoxInfo">IBoxInfo`s</see>
+        /// 	<see cref="M:Ferda.Modules.Boxes.IBoxInfo.GetBoxModuleFunctionsIceIds()"/>.
+        /// </example>
         public override string[] GetBoxModuleFunctionsIceIds()
         {
             return Functions.ids__;
         }
 
+        /// <summary>
+        /// Gets default value for box module user label.
+        /// </summary>
+        /// <param name="boxModule">A module that returns the label</param>
+        /// <returns>The user label</returns>
         public override string GetDefaultUserLabel(BoxModuleI boxModule)
         {
             return null;
         }
 
+        /// <summary>
+        /// Gets the box modules asking for creation.
+        /// </summary>
+        /// <param name="localePrefs">The localization preferences.</param>
+        /// <param name="boxModule">The box module.</param>
+        /// <returns>
+        /// Array of <see cref="T:Ferda.Modules.ModuleAskingForCreation">
+        /// Modules Asking For Creation</see>.
+        /// </returns>
         public override ModulesAskingForCreation[] GetModulesAskingForCreation(string[] localePrefs,
                                                                                BoxModuleI boxModule)
         {
             return new ModulesAskingForCreation[0];
         }
 
+        /// <summary>
+        /// Gets array of <see cref="T:Ferda.Modules.SelectString"/> as
+        /// options for property, whose options are dynamically variable.
+        /// </summary>
+        /// <param name="boxModule">The current module</param>
+        /// <param name="propertyName">Name of the property</param>
+        /// <returns>String options of the property</returns>
         public override SelectString[] GetPropertyOptions(string propertyName, BoxModuleI boxModule)
         {
             return null;
         }
 
-        public const string typeIdentifier = "GuhaMining.Tasks.SDSingleDimensional";
-
-        protected override string identifier
-        {
-            get { return typeIdentifier; }
-        }
-
+        /// <summary>
+        /// Gets value of readonly property value.
+        /// </summary>
+        /// <param name="propertyName">Name of readonly property.</param>
+        /// <param name="boxModule">Box module.</param>
+        /// <returns>
+        /// A <see cref="T:Ferda.Modules.PropertyValue"/> of
+        /// readonly property named <c>propertyName</c>.
+        /// </returns>
         public override PropertyValue GetReadOnlyPropertyValue(string propertyName, BoxModuleI boxModule)
         {
             Functions Func = (Functions) boxModule.FunctionsIObj;
@@ -60,6 +125,17 @@ namespace Ferda.Modules.Boxes.GuhaMining.Tasks.SDSingleDimensional
             }
         }
 
+        /// <summary>
+        /// Executes (runs) action specified by <c>actionName</c>.
+        /// </summary>
+        /// <param name="actionName">Name of the action.</param>
+        /// <param name="boxModule">The Box module.</param>
+        /// <exception cref="T:Ferda.Modules.NameNotExistError">
+        /// Thrown if action named <c>actionName</c> doesn`t exist.
+        /// </exception>
+        /// <exception cref="T:Ferda.Modules.BoxRuntimeError">
+        /// Thrown if any runtime error occured while executing the action.
+        /// </exception>
         public override void RunAction(string actionName, BoxModuleI boxModule)
         {
             Functions Func = (Functions) boxModule.FunctionsIObj;
@@ -73,6 +149,10 @@ namespace Ferda.Modules.Boxes.GuhaMining.Tasks.SDSingleDimensional
             }
         }
 
+        /// <summary>
+        /// Validates the box
+        /// </summary>
+        /// <param name="boxModule">box instance to be validated</param>
         public override void Validate(BoxModuleI boxModule)
         {
             // all used attributes are from the same data table
@@ -80,5 +160,32 @@ namespace Ferda.Modules.Boxes.GuhaMining.Tasks.SDSingleDimensional
             Functions Func = (Functions) boxModule.FunctionsIObj;
             Func.GetSourceDataTableId();
         }
+
+        #region Type Identifier
+
+        /// <summary>
+        /// This is recomended (not required) to have <c>public const string</c> 
+        /// field in the BoxInfo implementation which holds the identifier 
+        /// of type of the box module.
+        /// </summary>
+        public const string typeIdentifier = "GuhaMining.Tasks.SDSingleDimensional";
+
+        /// <summary>
+        /// Unique identifier of type of Box module
+        /// </summary>
+        /// <value></value>
+        /// <remarks>
+        /// This string identifier is parsed i.e. dots (".") are
+        /// replaced by <see cref="P:System.IO.Path.DirectorySeparatorChar"/>.
+        /// Returned path is combined with application directory`s
+        /// <see cref="F:Ferda.Modules.Boxes.BoxInfo.configFilesFolderName">subdirectory</see>
+        /// and final path is used for getting stored configuration [localization] XML files.
+        /// </remarks>
+        protected override string identifier
+        {
+            get { return typeIdentifier; }
+        }
+
+        #endregion
     }
 }
