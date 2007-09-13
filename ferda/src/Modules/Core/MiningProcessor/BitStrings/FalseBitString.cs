ea12edccf3c22d1bcc77ae6988e1d3e09c961743
kanec_martin <kanec_martin@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2007-09-13 21:06:05 +0000

    Encoding commit

--- ferda/src/Modules/Core/MiningProcessor/BitStrings/FalseBitString.cs
+++ ferda/src/Modules/Core/MiningProcessor/BitStrings/FalseBitString.cs
@@ -1,1 +1,185 @@
-// FalseBitString.cs - Singleton false empty bit string // // Author: Tomáš Kuchař <tomas.kuchar@gmail.com> // Commented by: Martin Ralbovský <martin.ralbovsky@gmail.com> // // Copyright (c) 2006 Tomáš Kuchař // // This program is free software; you can redistribute it and/or modify // it under the terms of the GNU General Public License as published by // the Free Software Foundation; either version 2 of the License, or // (at your option) any later version. // // This program is distributed in the hope that it will be useful, // but WITHOUT ANY WARRANTY; without even the implied warranty of // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the // GNU General Public License for more details. // // You should have received a copy of the GNU General Public License // along with this program; if not, write to the Free Software // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  using System; using Ferda.Guha.MiningProcessor.Formulas;  namespace Ferda.Guha.MiningProcessor.BitStrings {     /// <summary>     /// Singleton class that represents a false bit string - bit string that     /// is zero for any length. For performance     /// reasons, we chose to have only one false bit string.     /// </summary>     public class FalseBitString : IEmptyBitString     {         #region Singleton          /// <summary>         /// The singleton (one) object that can be created         /// </summary>         private static readonly FalseBitString _instance = new FalseBitString();          /// <summary>         /// Object used for thread-safe access to the bit string cache         /// </summary>         private static readonly object padlock = new object();          /// <summary>         /// Explicit static constructor to tell C# compiler         /// not to mark type as beforefieldinit         /// </summary>         static FalseBitString()         {         }          /// <summary>         /// Gets the (singleton) instance of the false bit string.          /// </summary>         /// <returns>Instance of the false bit string</returns>         public static FalseBitString GetInstance()         {             lock (padlock)             {                 return _instance;             }         }          #endregion          /// <summary>         /// The false bit string identifier         /// </summary>         public static readonly BitStringIdentifier FalseBitStringIdentifier =             new BitStringIdentifier(null, "False BitString");          /// <summary>         /// Boolean attribute formula for the false bit string         /// </summary>         public static readonly BooleanAttributeFormula FalseBitStringId = new AtomFormula(FalseBitStringIdentifier);          #region IBitString Members          /// <summary>         /// Identifier of the bit string (each bit string should be         /// identified by a boolean attribute formula representing the         /// bit string.          /// </summary>         public BooleanAttributeFormula Identifier         {             get { return FalseBitStringId; }         }          /// <summary>         /// Performs the bitwise AND operation on current BitString against the specified BitString.         /// </summary>         /// <param name="source">The second BitString operand.</param>         public IBitString And(IBitString source)         {             return this;         }          ///// <summary>         ///// Performs the bitwise AND operation on         ///// copy of current BitString against the specified BitString.         ///// </summary>         ///// <param name="source">The second BitString operand.</param>         public IBitString AndCloned(IBitString source)         {             return this;         }          /// <summary>         /// Performs a NOT operation on the empty bit string (result is         /// again an empty bit string.         /// </summary>         /// <returns>Result of NOT operation</returns>         public IBitString Not()         {             throw new NotSupportedException("Negation of false bit string is not supported.");         }          ///// <summary>         ///// Performs the bitwise NOT on copy of current BitString.         ///// </summary>         public IBitString NotCloned()         {             throw new NotSupportedException("Negation of false bit string is not supported.");         }          /// <summary>         /// Special OR operation for handling empty and false bit strings         /// </summary>         /// <param name="source">Source bit string</param>         /// <returns>Result of special OR operation</returns>         public IBitString orSpecial(IBitString source)         {             if (source is EmptyBitString)                 return source;             else if (source is FalseBitString)                 return this;             else                 throw new NotImplementedException();         }          /// <summary>         /// Performs the bitwise OR operation on current BitString against the specified BitString.         /// </summary>         /// <param name="source">The second BitString operand.</param>         public IBitString Or(IBitString source)         {             return source;         }          //public IBitString OrCloned(IBitString source)         //{         //    BitString bs = source as BitString;         //    if (bs != null)         //        return bs.OrCloned(this);         //    else         //        return orSpecial(source, false);         //}          /// <summary>         /// Performs the bitwise SUM operation on current BitString.         /// </summary>         /// <returns>The number of bits set to 1 in current BitString.</returns>         public int Sum         {             get { return 0; } 			set {}         }          #endregion          #region IBitStringBase Members          /// <summary>         /// Length of a bit string         /// </summary>         public int Length         {             get { return 1; }         }          #endregion     } } 
+// FalseBitString.cs - Singleton false empty bit string
+//
+// Author: Tomáš Kuchař <tomas.kuchar@gmail.com>
+// Commented by: Martin Ralbovský <martin.ralbovsky@gmail.com>
+//
+// Copyright (c) 2006 Tomáš Kuchař
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+using System;
+using Ferda.Guha.MiningProcessor.Formulas;
+
+namespace Ferda.Guha.MiningProcessor.BitStrings
+{
+    /// <summary>
+    /// Singleton class that represents a false bit string - bit string that
+    /// is zero for any length. For performance
+    /// reasons, we chose to have only one false bit string.
+    /// </summary>
+    public class FalseBitString : IEmptyBitString
+    {
+        #region Singleton
+
+        /// <summary>
+        /// The singleton (one) object that can be created
+        /// </summary>
+        private static readonly FalseBitString _instance = new FalseBitString();
+
+        /// <summary>
+        /// Object used for thread-safe access to the bit string cache
+        /// </summary>
+        private static readonly object padlock = new object();
+
+        /// <summary>
+        /// Explicit static constructor to tell C# compiler
+        /// not to mark type as beforefieldinit
+        /// </summary>
+        static FalseBitString()
+        {
+        }
+
+        /// <summary>
+        /// Gets the (singleton) instance of the false bit string. 
+        /// </summary>
+        /// <returns>Instance of the false bit string</returns>
+        public static FalseBitString GetInstance()
+        {
+            lock (padlock)
+            {
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The false bit string identifier
+        /// </summary>
+        public static readonly BitStringIdentifier FalseBitStringIdentifier =
+            new BitStringIdentifier(null, "False BitString");
+
+        /// <summary>
+        /// Boolean attribute formula for the false bit string
+        /// </summary>
+        public static readonly BooleanAttributeFormula FalseBitStringId = new AtomFormula(FalseBitStringIdentifier);
+
+        #region IBitString Members
+
+        /// <summary>
+        /// Identifier of the bit string (each bit string should be
+        /// identified by a boolean attribute formula representing the
+        /// bit string. 
+        /// </summary>
+        public BooleanAttributeFormula Identifier
+        {
+            get { return FalseBitStringId; }
+        }
+
+        /// <summary>
+        /// Performs the bitwise AND operation on current BitString against the specified BitString.
+        /// </summary>
+        /// <param name="source">The second BitString operand.</param>
+        public IBitString And(IBitString source)
+        {
+            return this;
+        }
+
+        ///// <summary>
+        ///// Performs the bitwise AND operation on
+        ///// copy of current BitString against the specified BitString.
+        ///// </summary>
+        ///// <param name="source">The second BitString operand.</param>
+        public IBitString AndCloned(IBitString source)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// Performs a NOT operation on the empty bit string (result is
+        /// again an empty bit string.
+        /// </summary>
+        /// <returns>Result of NOT operation</returns>
+        public IBitString Not()
+        {
+            throw new NotSupportedException("Negation of false bit string is not supported.");
+        }
+
+        ///// <summary>
+        ///// Performs the bitwise NOT on copy of current BitString.
+        ///// </summary>
+        public IBitString NotCloned()
+        {
+            throw new NotSupportedException("Negation of false bit string is not supported.");
+        }
+
+        /// <summary>
+        /// Special OR operation for handling empty and false bit strings
+        /// </summary>
+        /// <param name="source">Source bit string</param>
+        /// <returns>Result of special OR operation</returns>
+        public IBitString orSpecial(IBitString source)
+        {
+            if (source is EmptyBitString)
+                return source;
+            else if (source is FalseBitString)
+                return this;
+            else
+                throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Performs the bitwise OR operation on current BitString against the specified BitString.
+        /// </summary>
+        /// <param name="source">The second BitString operand.</param>
+        public IBitString Or(IBitString source)
+        {
+            return source;
+        }
+
+        //public IBitString OrCloned(IBitString source)
+        //{
+        //    BitString bs = source as BitString;
+        //    if (bs != null)
+        //        return bs.OrCloned(this);
+        //    else
+        //        return orSpecial(source, false);
+        //}
+
+        /// <summary>
+        /// Performs the bitwise SUM operation on current BitString.
+        /// </summary>
+        /// <returns>The number of bits set to 1 in current BitString.</returns>
+        public int Sum
+        {
+            get { return 0; }
+			set {}
+        }
+
+        #endregion
+
+        #region IBitStringBase Members
+
+        /// <summary>
+        /// Length of a bit string
+        /// </summary>
+        public int Length
+        {
+            get { return 1; }
+        }
+
+        #endregion
+    }
+}
