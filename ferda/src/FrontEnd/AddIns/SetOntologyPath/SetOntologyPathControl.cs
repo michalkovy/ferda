c134300cb1952a5d8a89683252a21cd3acd53552
marthin5 <marthin5@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2007-10-12 09:37:53 +0000

    Ontology related boxes committed (v 0.15 - no functionality yet)

--- ferda/src/FrontEnd/AddIns/SetOntologyPath/SetOntologyPathControl.cs
+++ ferda/src/FrontEnd/AddIns/SetOntologyPath/SetOntologyPathControl.cs
@@ -10,6 +10,46 @@ namespace Ferda.FrontEnd.AddIns.SetOntologyPath
 {
     public partial class SetOntologyPathControl : Form
     {
+
+        #region Private variables
+
+        /// <summary>
+        /// Localization string - for now, en-US or cs-CZ
+        /// </summary>
+        private string localizationString;
+
+        /// <summary>
+        /// Resulting Ontology Path
+        /// </summary>
+        private string returnOntologyPath;
+
+        /// <summary>
+        /// Current path (for icon loading)
+        /// </summary>
+        private string path;
+        /*
+        /// <summary>
+        /// Owner of addin
+        /// </summary>
+        private IOwnerOfAddIn ownerOfAddIn;
+        */
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Resulting DSN
+        /// </summary>
+        public string ReturnOntologyPath
+        {
+            get
+            {
+                return returnOntologyPath;
+            }
+        }
+
+        #endregion
+
         public SetOntologyPathControl()
         {
             InitializeComponent();
@@ -19,6 +59,50 @@ namespace Ferda.FrontEnd.AddIns.SetOntologyPath
         {
             openFileDialog1.FileName = "";
             openFileDialog1.ShowDialog();
+            diskPathTextBox.Text = openFileDialog1.FileName;
+            radioButton1.Checked = true;
+            radioButton2.Checked = false;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                this.returnOntologyPath = diskPathTextBox.Text;
+                this.DialogResult = DialogResult.OK;
+                this.Dispose();
+                return;
+            }
+            else if (radioButton2.Checked)
+            {
+                this.returnOntologyPath = URLPathTextBox.Text;
+                this.DialogResult = DialogResult.OK;
+                this.Dispose();
+                return;
+            }
+            else
+            {
+                this.DialogResult = DialogResult.Cancel;
+                this.Dispose();
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Dispose();
+        }
+
+        private void URLPathTextBox_TextChanged(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = true;
+        }
+
+        private void diskPathTextBox_TextChanged(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+            radioButton2.Checked = false;
         }
 
     }
