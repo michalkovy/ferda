f665da28bd601d8e0c999a21484d044e3de8edaa
kanec_martin <martin.ralbovsky@gmail.com>
2007-11-30 10:07:07 +0000

    Equidistant attributes functional for string and time data types.

--- ferda/src/Modules/BoxModulesServices/DataPreparation/Categorization/EquidistantIntervalsLISp/Functions.cs
+++ ferda/src/Modules/BoxModulesServices/DataPreparation/Categorization/EquidistantIntervalsLISp/Functions.cs
@@ -537,7 +537,6 @@ namespace Ferda.Modules.Boxes.DataPreparation.Categorization.EquidistantInterval
 
                             case DbSimpleDataTypeEnum.StringSimpleType:
                                 throw new Exception("Cannot create equidistant intervals from a column containing strings.\nFerda cannot determine how to measure distance between strings.");
-                                break;
 
                             case DbSimpleDataTypeEnum.ShortSimpleType:
                             case DbSimpleDataTypeEnum.IntegerSimpleType:
