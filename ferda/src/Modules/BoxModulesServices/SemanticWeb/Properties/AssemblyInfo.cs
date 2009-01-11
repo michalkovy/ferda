2ffa1cc8dddf37ce97342cbe1342498feddf6dba
kanec_martin <kanec_martin@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2009-01-11 20:11:09 +0000

    PMMLBuilder box - progress

---
+++ ferda/src/Modules/BoxModulesServices/SemanticWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FerdaSemanticWebBoxes")]
+[assembly: AssemblyDescription("Boxes for deployment to semantic web")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KIZI UEP Prague")]
+[assembly: AssemblyProduct("Ferda")]
+[assembly: AssemblyCopyright("(c) KIZI UEP Prague")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ff2b12c-7ea6-4f06-bc43-a6cf840bc1f5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
