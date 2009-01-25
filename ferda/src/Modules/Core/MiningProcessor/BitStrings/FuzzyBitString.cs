81b7f68991fddf26e7167c8bdaee0f944457a236
kanec_martin <martin.ralbovsky@gmail.com>
2009-01-25 16:39:40 +0000

    Added the FuzzyBitString.cs + test project for fuzzy bit strings.

---
+++ ferda/src/Modules/Core/MiningProcessor/BitStrings/FuzzyBitString.cs
@@ -0,0 +1,218 @@
+﻿// BitString.cs - fuzzy BitString of a fixed length
+//
+// Authors: Martin Ralbovský <martin.ralbovsky@gmail.com>
+//
+// Copyright (c) 2009 Martin Ralbovský 
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ferda.Guha.MiningProcessor.Formulas;
+using Mono.Simd;
+
+namespace Ferda.Guha.MiningProcessor.BitStrings
+{
+    public class FuzzyBitString : IBitStringBase
+    {
+        /// <summary>
+        /// Internal array where fuzzy bit strings are stored
+        /// </summary>
+        private Vector4f[] _array;
+
+        /// <summary>
+        /// The size of the bit string
+        /// </summary>
+        private int _size;
+
+        /// <summary>
+        /// Identifier of the bit string (each bit string should be
+        /// identified by a boolean attribute formula representing the
+        /// bit string. 
+        /// </summary>
+        private BooleanAttributeFormula _identifier;
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the length of the BitString.
+        /// </summary>
+        public int Length
+        {
+            get { return _size; }
+        }
+
+        /// <summary>
+        /// Identifier of the bit string (each bit string should be
+        /// identified by a boolean attribute formula representing the
+        /// bit string. 
+        /// </summary>
+        public BooleanAttributeFormula Identifier
+        {
+            get { return _identifier; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Adds the identifier
+        /// </summary>
+        /// <param name="identifier">Identifier</param>
+        private FuzzyBitString(BooleanAttributeFormula identifier)
+        {
+            _identifier = identifier;
+        }
+
+        /// <summary>
+        /// Copy constructor
+        /// </summary>
+        /// <param name="source">The source bit string (from which it is copied)</param>
+        public FuzzyBitString(FuzzyBitString source)
+            : this(source._identifier)
+        {
+            if (source._size == 0)
+                throw new InvalidOperationException("Cannot copy-construct a BitString from an uninitialized one.");
+
+            _size = source._size;
+            _array = new Vector4f[source._array.Length];
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = new Vector4f(source._array[i].X, source._array[i].Y, source._array[i].Z, source._array[i].W);
+            }
+        }
+
+        /// <summary>
+        /// Constructor of the class.
+        /// </summary>
+        /// <param name="identifier">Identifier of the bit string</param>
+        /// <param name="floats">Array of floats, which are transformed into
+        /// the <see cref="Mono.Simd.Vector4f"/>structures.</param>
+        /// <remarks>
+        /// In contrary to the constructor of <see cref="BitString"/> class, this constructor
+        /// is safe and it does not need the length parameter. The length of the bit string
+        /// is determined by the lenght of the array of floats. 
+        /// </remarks>
+        public FuzzyBitString(BitStringIdentifier identifier, float[] floats)
+            : this(new AtomFormula(identifier))
+        {
+            if (floats.Length == 0)
+            {
+                throw new Exception();
+                //throw Exceptions.BitStringLengthError();
+            }
+
+            _size = floats.Length;
+
+            // if size mod 4 = 0, the the vectorF size = size/4 else size/4 + 1
+            int flength = (_size % 4 == 0) ? _size / 4 : (_size / 4) + 1;
+            _array = new Vector4f[flength];
+
+            for (int i = 0; i < flength; i++)
+            {
+                //so it is sure, that there are 4 floats still to be filled
+                if (4 * (i + 1) <= floats.Length)
+                {
+                    _array[i] = new Vector4f(floats[4 * i],
+                        floats[4 * i + 1], floats[4 * i + 2], floats[4 * i + 3]);
+                }
+                else
+                {
+                    //there is no other float to be filled
+                    if (4 * i == floats.Length + 1)
+                    {
+                        break;
+                    }
+                    //there is at least one float to be filled
+                    else
+                    {
+                        _array[i] = new Vector4f();
+                        _array[i].X = floats[4 * i];
+                        //there is only one
+                        if (floats.Length % 4 == 1)
+                        {
+                            _array[i].Y = -1f;
+                            _array[i].Z = -1f;
+                            _array[i].W = -1f;
+                            break;
+                        }
+                        //there are at least 2 floats to be filled
+                        if (floats.Length % 4 == 2)
+                        {
+                            _array[i].Y = floats[4 * i + 1];
+                            _array[i].Z = -1f;
+                            _array[i].W = -1f;
+                        }
+                        else
+                        {
+                            _array[i].Y = floats[4 * i + 1];
+                            _array[i].Z = floats[4 * i + 2];
+                            _array[i].W = -1f;
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region And
+
+        #endregion
+
+        /// <summary>
+        /// Returns the human-readable string of the BitString contents.
+        /// </summary>
+        /// <returns>Human-readable string of the BitString contents.</returns>
+        public override string ToString()
+        {
+            if (_size > 0)
+            {
+                StringBuilder retValue = new StringBuilder();
+                //Adding all the Vector4f's that are sure to be full
+                for (int i = 0; i < _array.Length - 1; i++)
+                {
+                    retValue.AppendFormat("|{0:F3}|{1:F3}|{2:F3}|{3:F3}|",
+                        _array[i].X, _array[i].Y, _array[i].Z, _array[i].W);
+                }
+                //Adding the last Vector4f - there has to be at least one item
+                retValue.AppendFormat("{0:F3}|", _array[_array.Length - 1].X);
+
+                if (_size % 4 == 2)
+                {
+                    retValue.AppendFormat("{0:F3}|", _array[_array.Length - 1].Y);
+                }
+                if (_size % 4 == 3)
+                {
+                    retValue.AppendFormat("{0:F3}|{1:F3}|",
+                        _array[_array.Length - 1].Y, _array[_array.Length - 1].Z);
+                }
+                if (_size % 4 == 0)
+                {
+                    retValue.AppendFormat("{0:F3}|{1:F3}|{2:F3}|",
+                        _array[_array.Length - 1].Y, _array[_array.Length - 1].Z, _array[_array.Length - 1].W);
+                }
+                return retValue.ToString();
+            }
+            else
+            {
+                return "uninitialized fuzzy BitString";
+            }
+        }
+    }
+}
---
+++ ferda/test/FuzzyBitStringsTest/FuzzyBitStringTest.cs
@@ -0,0 +1,125 @@
+﻿// FuzzyBitStringsTest.cs - NUnit tests for Fuzzy bit strings
+//
+// Author: Martin Ralbovský <martin.ralbovsky@gmail.cz>
+//
+// Copyright (c) 2009 Martin Ralbovský
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using NUnit.Framework;
+using Ferda.Guha.MiningProcessor.BitStrings;
+
+namespace FuzzyBitStringsTest
+{
+    /// <summary>
+    /// Test of functionality of bit strings
+    /// </summary>
+    [TestFixture]
+    public class FuzzyBitStringTest
+    {
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            Debug.Listeners.Clear();
+            TextWriterTraceListener t = new TextWriterTraceListener("log.txt");
+            Debug.Listeners.Add(t);
+            Debug.AutoFlush = true;
+            Debug.WriteLine("The fuzzy bit strings test...");
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Debug.WriteLine("End of fuzzy bit strings test");
+        }
+
+        [Test]
+        public void ConstructorAndToStringTest()
+        {
+            Debug.WriteLine("Constructor and ToString() test");
+            BitStringIdentifier o = new BitStringIdentifier("x", "y");
+
+            float f1 = 0.01f;
+            float f2 = 0.02f;
+            float f3 = 0.03f;
+            float f4 = 0.04f;
+            float f5 = 0.05f;
+            float f6 = 0.06f;
+            float f7 = 0.07f;
+            float f8 = 0.08f;
+            float f9 = 0.09f;
+            float f10 = 0.1f;
+            float f11 = 0.11f;
+            float f12 = 0.12f;
+
+            float[] fa1 = new float[] { f1 };
+            float[] fa2 = new float[] { f1, f2 };
+            float[] fa3 = new float[] { f1, f2, f3 };
+            float[] fa4 = new float[] { f1, f2, f3, f4 };
+            float[] fa5 = new float[] { f1, f2, f3, f4, f5 };
+            float[] fa6 = new float[] { f1, f2, f3, f4, f5, f6 };
+            float[] fa7 = new float[] { f1, f2, f3, f4, f5, f6, f7 };
+            float[] fa8 = new float[] { f1, f2, f3, f4, f5, f6, f7, f8 };
+            float[] fa9 = new float[] { f1, f2, f3, f4, f5, f6, f7, f8, f9 };
+            float[] fa10 = new float[] { f1, f2, f3, f4, f5, f6, f7, f8, f9, f10 };
+            float[] fa11 = new float[] { f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11 };
+            float[] fa12 = new float[] { f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12 };
+
+            FuzzyBitString fb1 = new FuzzyBitString(o, fa1);
+            FuzzyBitString fb2 = new FuzzyBitString(o, fa2);
+            FuzzyBitString fb3 = new FuzzyBitString(o, fa3);
+            FuzzyBitString fb4 = new FuzzyBitString(o, fa4);
+            FuzzyBitString fb5 = new FuzzyBitString(o, fa5);
+            FuzzyBitString fb6 = new FuzzyBitString(o, fa6);
+            FuzzyBitString fb7 = new FuzzyBitString(o, fa7);
+            FuzzyBitString fb8 = new FuzzyBitString(o, fa8);
+            FuzzyBitString fb9 = new FuzzyBitString(o, fa9);
+            FuzzyBitString fb10 = new FuzzyBitString(o, fa10);
+            FuzzyBitString fb11 = new FuzzyBitString(o, fa11);
+            FuzzyBitString fb12 = new FuzzyBitString(o, fa12);
+
+            Debug.WriteLine(fb1.ToString());
+            Debug.WriteLine(fb1.Length);
+            Debug.WriteLine(fb2.ToString());
+            Debug.WriteLine(fb2.Length);
+            Debug.WriteLine(fb3.ToString());
+            Debug.WriteLine(fb3.Length);
+            Debug.WriteLine(fb4.ToString());
+            Debug.WriteLine(fb4.Length);
+            Debug.WriteLine(fb5.ToString());
+            Debug.WriteLine(fb5.Length);
+            Debug.WriteLine(fb6.ToString());
+            Debug.WriteLine(fb6.Length);
+            Debug.WriteLine(fb7.ToString());
+            Debug.WriteLine(fb7.Length);
+            Debug.WriteLine(fb8.ToString());
+            Debug.WriteLine(fb8.Length);
+            Debug.WriteLine(fb9.ToString());
+            Debug.WriteLine(fb9.Length);
+            Debug.WriteLine(fb10.ToString());
+            Debug.WriteLine(fb10.Length);
+            Debug.WriteLine(fb11.ToString());
+            Debug.WriteLine(fb11.Length);
+            Debug.WriteLine(fb12.ToString());
+            Debug.WriteLine(fb12.Length);
+            Debug.WriteLine("Constructor and ToString() test successful");
+        }
+    }
+}
