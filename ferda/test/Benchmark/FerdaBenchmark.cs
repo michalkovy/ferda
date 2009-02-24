fbb45839e9f6730078f5b7fed72f822bf56e5b20
kanec_martin <martin.ralbovsky@gmail.com>
2009-02-24 15:26:56 +0000

    The benchmark for disjunction added.

--- ferda/test/Benchmark/FerdaBenchmark.cs
+++ ferda/test/Benchmark/FerdaBenchmark.cs
@@ -25,7 +25,7 @@ namespace Ferda.Benchmark
     {
         #region Fields
 
-        protected static int iterations = 10000;
+        protected static int iterations = 1000;
         protected const int lengthUlongString = 100000;
 
         protected static ulong[] stringUlong = new ulong[lengthUlongString];
@@ -70,6 +70,30 @@ namespace Ferda.Benchmark
         {
             get { return lengthUlongString * 64;  }
         }
+
+        /// <summary>
+        /// The ULONG representing 1
+        /// </summary>
+        protected const ulong _one = 1ul;
+
+        //uint precomputed vectors
+        protected static Vector4ui sui1 = new Vector4ui(0x00000001, 0x00000002, 0x00000004, 0x00000008);
+        protected static Vector4ui sui2 = new Vector4ui(0x00000010, 0x00000020, 0x00000040, 0x00000080);
+        protected static Vector4ui sui3 = new Vector4ui(0x00000100, 0x00000200, 0x00000400, 0x00000800);
+        protected static Vector4ui sui4 = new Vector4ui(0x00001000, 0x00002000, 0x00004000, 0x00008000);
+        protected static Vector4ui sui5 = new Vector4ui(0x00010000, 0x00020000, 0x00040000, 0x00080000);
+        protected static Vector4ui sui6 = new Vector4ui(0x00100000, 0x00200000, 0x00400000, 0x00800000);
+        protected static Vector4ui sui7 = new Vector4ui(0x01000000, 0x02000000, 0x04000000, 0x08000000);
+        protected static Vector4ui sui8 = new Vector4ui(0x10000000, 0x20000000, 0x40000000, 0x80000000);
+        //float precomputed vectors
+        protected static Vector4f sf1 = new Vector4f(0x00000001, 0x00000002, 0x00000004, 0x00000008);
+        protected static Vector4f sf2 = new Vector4f(0x00000010, 0x00000020, 0x00000040, 0x00000080);
+        protected static Vector4f sf3 = new Vector4f(0x00000100, 0x00000200, 0x00000400, 0x00000800);
+        protected static Vector4f sf4 = new Vector4f(0x00001000, 0x00002000, 0x00004000, 0x00008000);
+        protected static Vector4f sf5 = new Vector4f(0x00010000, 0x00020000, 0x00040000, 0x00080000);
+        protected static Vector4f sf6 = new Vector4f(0x00100000, 0x00200000, 0x00400000, 0x00800000);
+        protected static Vector4f sf7 = new Vector4f(0x01000000, 0x02000000, 0x04000000, 0x08000000);
+        protected static Vector4f sf8 = new Vector4f(0x10000000, 0x20000000, 0x40000000, 0x80000000);
 
         #endregion
 
