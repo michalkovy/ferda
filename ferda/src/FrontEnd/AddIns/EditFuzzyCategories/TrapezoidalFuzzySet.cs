1968e21fab48adb9285dfbf94e630e16bc4fb9e0
kanec_martin <martin.ralbovsky@gmail.com>
2009-03-05 21:35:32 +0000

    The graph displays fuzzy sets in EditFuzzyCategories

---
+++ ferda/src/FrontEnd/AddIns/EditFuzzyCategories/TrapezoidalFuzzySet.cs
@@ -0,0 +1,59 @@
+﻿// TrapezoidalFuzzySet.cs - Simple representation of the trapezoidal fuzzy set
+//
+// Author: Martin Ralbovsky <martin.ralbovsky@gmail.com>
+//
+// Copyright (c) 2009 Martin Ralbovsky
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ferda.FrontEnd.AddIns.EditFuzzyCategories
+{
+    /// <summary>
+    /// A simplified representation of a trapezoidal fuzzy set. Only 4 points
+    /// and a name of the fuzzy set is stored
+    /// </summary>
+    public struct TrapezoidalFuzzySet
+    {
+        /// <summary>
+        /// Name of the fuzzy set
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// A (start of the trapezoid)
+        /// </summary>
+        public double A;
+
+        /// <summary>
+        /// B (end of the trapezoid)
+        /// </summary>
+        public double B;
+
+        /// <summary>
+        /// C (descending peak)
+        /// </summary>
+        public double C;
+
+        /// <summary>
+        /// D (ascending peak)
+        /// </summary>
+        public double D;
+    }
+}
