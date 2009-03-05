c716eac33d673cf549ec22141c9cd282b82d961c
kanec_martin <kanec_martin@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2009-03-05 21:35:32 +0000

    The graph displays fuzzy sets in EditFuzzyCategories

--- ferda/src/FrontEnd/AddIns/EditFuzzyCategories/MainWindow.cs
+++ ferda/src/FrontEnd/AddIns/EditFuzzyCategories/MainWindow.cs
@@ -26,6 +26,7 @@ using System.Resources;
 using System.Text;
 using System.Windows.Forms;
 using Steema.TeeChart;
+using Steema.TeeChart.Styles;
 
 namespace Ferda.FrontEnd.AddIns.EditFuzzyCategories
 {
@@ -62,11 +63,22 @@ namespace Ferda.FrontEnd.AddIns.EditFuzzyCategories
         /// <param name="maximum">Maximal value of the attribute</param>
         public MainWindow(ResourceManager resManager, double minimum, double maximum)
         {
+            this.minimum = minimum;
+            this.maximum = maximum;
             resourceManager = resManager;
             InitializeComponent();
             InitializeGraph();
 
             ChangeLocale();
+            List<TrapezoidalFuzzySet> list = new List<TrapezoidalFuzzySet>();
+            TrapezoidalFuzzySet f = new TrapezoidalFuzzySet();
+            f.Name = "name";
+            f.A = 10000;
+            f.D = 10500;
+            f.C = 11000;
+            f.B = 11500;
+            list.Add(f);
+            FillGraph(list);
         }
 
         /// <summary>
@@ -77,9 +89,68 @@ namespace Ferda.FrontEnd.AddIns.EditFuzzyCategories
             graph = new TChart();
             graph.Location = new System.Drawing.Point(12, 12);
             graph.Aspect.View3D = false;
+            graph.Header.Visible = false;
+            graph.Zoom.Allow = false;
+            graph.Panel.MarginBottom = 2;
+            graph.Panel.MarginLeft = 2;
+            graph.Panel.MarginRight = 2;
+            graph.Panel.MarginTop = 2;
             graph.Size = new System.Drawing.Size(822, 236);
+            graph.Axes.Left.Automatic = false;
+            graph.Axes.Left.Minimum = 0;
+            graph.Axes.Left.Maximum = 1.1;
+            graph.Axes.Bottom.Automatic = false;
+            graph.Axes.Bottom.Minimum = minimum;
+            graph.Axes.Bottom.Maximum = maximum;
+            graph.Axes.Visible = true;
             this.Controls.Add(graph);
+            graph.ResumeLayout();
             this.ResumeLayout();
+        }
+
+        /// <summary>
+        /// Fills the graph with fuzzy sets. 
+        /// </summary>
+        /// <param name="fuzzySets">Fuzzy sets in trapeziodal form</param>
+        private void FillGraph(IList<TrapezoidalFuzzySet> fuzzySets)
+        {
+            Random r = new Random();
+            graph.Series.RemoveAllSeries();
+            foreach (TrapezoidalFuzzySet fuzzySet in fuzzySets)
+            {
+                //System.Drawing.Color c =
+                //    System.Drawing.Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
+
+                Line l = new Line();
+                l.Title = fuzzySet.Name;
+                //l.Brush.Color = c;
+                l.Brush.Color = System.Drawing.Color.Red;
+                l.Add(fuzzySet.A,0);
+                l.Add(fuzzySet.D,1);
+                l.Add(fuzzySet.C,1);
+                l.Add(fuzzySet.B,0);
+                graph.Series.Add(l);
+            }
+
+
+            //graph.Series.RemoveAllSeries();
+            //Line l = new Line();
+            //l.Title = "lajna2";
+            //l.Brush.Color = System.Drawing.Color.Red;
+            //l.Add(10000, 0);
+            //l.Add(10500, 1);
+            //l.Add(11000, 1);
+            //l.Add(11500, 0);
+            //graph.Series.Add(l);
+            
+            //l = new Line();
+            //l.Title = "lajna1";
+            //l.Brush.Color = System.Drawing.Color.Blue;
+            //l.Add(9000, 0);
+            //l.Add(9500, 1);
+            //l.Add(10000, 1);
+            //l.Add(10500, 0);
+            //graph.Series.Add(l);
         }
 
         /// <summary>
