0a0eede92ba325ddf9ded98674227c27c880b4cc
kanec_martin <martin.ralbovsky@gmail.com>
2009-09-14 15:58:02 +0000

    The SEWEBAR Publisher application should be ready to use

--- ferda/src/Utils/SewebarPublisher/Form1.cs
+++ ferda/src/Utils/SewebarPublisher/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Sewebar;
 
 namespace SewebarPublisher
 {
@@ -38,8 +39,100 @@ namespace SewebarPublisher
             }
             CBXMLRPCHost.SelectedIndex = 0;
 
+            //adding the name and password of the trial student (can be removed)
+            TBUserName.Text = "admin";
+            TBPassword.Text = "studentFIS";
+
+            LVArticles.Columns.Add("Article ID", -2, HorizontalAlignment.Left);
+            LVArticles.Columns.Add("Article title", -2, HorizontalAlignment.Left);
+
             this.ResumeLayout(false);
             this.PerformLayout();
+        }
+
+        /// <summary>
+        /// The event lists files of a particular user and fills them into 
+        /// the list view.
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
+        private void BListFiles_Click(object sender, EventArgs e)
+        {
+            LVArticles.Items.Clear();
+            IDictionary<int, string> files =
+                Sewebar.Sewebar.ListFiles(
+                    CBXMLRPCHost.SelectedItem.ToString(),
+                    TBUserName.Text, TBPassword.Text);
+
+            foreach (int key in files.Keys)
+            {
+                ListViewItem item = new ListViewItem(key.ToString());
+                item.SubItems.Add(files[key]);
+                LVArticles.Items.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// The event closes the form
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
+        private void BCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// The event publishes the content of the clipboard to SEWEBAR
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
+        private void BPublish_Click(object sender, EventArgs e)
+        {
+            if (!Clipboard.ContainsText())
+            {
+                MessageBox.Show("The system clipboard does not contain text","Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+                return;
+            }
+
+            string pmml = Clipboard.GetText();
+
+            if (LVArticles.Items.Count == 0 && TBNewArticle.Text == string.Empty)
+            {
+                MessageBox.Show("No article is selected","Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+                return;
+            }
+
+            if (LVArticles.SelectedItems.Count == 0 && TBNewArticle.Text == string.Empty)
+            {
+                MessageBox.Show("No article is selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            //retrieving the ID of the article to publish
+            int articleID = -1;
+            if (TBNewArticle.Text == string.Empty)
+            {
+                articleID = Convert.ToInt32(LVArticles.SelectedItems[0].Text);
+            }
+
+            //retrieving the title of the article to publish
+            string articleTitle = TBNewArticle.Text;
+            if (articleTitle == string.Empty)
+            {
+                articleTitle = LVArticles.SelectedItems[0].SubItems[1].Text;
+            }
+
+            string response = Sewebar.Sewebar.PublishToSewebar(
+                CBXMLRPCHost.SelectedItem.ToString(),
+                pmml,
+                TBUserName.Text,
+                TBPassword.Text,
+                articleTitle,
+                articleID);
+
+            MessageBox.Show(response, "Response");
+            this.Close();
         }
 
 
