c8b2885de2057e14c28edb36a4ad74040ff95395
Michal Kováč <16528591+michalkovy@users.noreply.github.com>
2010-05-26 16:13:17 +0000

    web service prepared

---
+++ ferda/src/WebService/FerdaCaller.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+
+namespace Ferda.WebService
+{
+    public partial class FerdaCaller : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void CallButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder pmmlBuilder = new StringBuilder();
+
+            System.IO.Stream stream = PmmlUpload.FileContent;
+            // Get the length of the file.
+            int fileLen = PmmlUpload.PostedFile.ContentLength;
+
+            // Create a byte array to hold the contents of the file.
+            Byte[] buffer = new Byte[fileLen];
+
+            stream.Read(buffer, 0, fileLen);
+
+            // Copy the byte array to a string.
+            for (int loop1 = 0; loop1 < fileLen; loop1++)
+            {
+                pmmlBuilder.Append(buffer[loop1].ToString());
+            }
+
+            FerdaService service = new FerdaService();
+            DataMiningResult result = service.MineDataWithPmmlSetUp(
+                new DataMiningRequest(DataProvider.Text,
+                    ConnectionString.Text,
+                    DataTableName.Text,
+                    pmmlBuilder.ToString()
+                    ));
+            Result.Text = result.ErrorMessage;
+        }
+    }
+}
