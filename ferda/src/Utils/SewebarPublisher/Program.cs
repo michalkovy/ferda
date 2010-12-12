2ab6632844e946c35e698c2df7c5db4b7aa9a2fc
kanec_martin <kanec_martin@e7fdecb9-3d0a-0410-9c94-8806e8c45573>
2010-12-12 16:33:24 +0000

    The program checks at startup, if the supporting libraries are not missing and then starts

--- ferda/src/Utils/SewebarPublisher/Program.cs
+++ ferda/src/Utils/SewebarPublisher/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
 
 namespace SewebarPublisher
 {
@@ -15,14 +16,45 @@ namespace SewebarPublisher
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            if (args != null && args.Length > 0)
+            
+            if (LibrariesPresent())
             {
-                Application.Run(new SEWEBARForm(args[0]));
+                if (args != null && args.Length > 0)
+                {
+                    Application.Run(new SEWEBARForm(args[0]));
+                }
+                else
+                {
+                    Application.Run(new SEWEBARForm());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks, if the libriaries with supporting SEWERAB and XMLRPC functions are present. 
+        /// </summary>
+        static Boolean LibrariesPresent()
+        {
+            Assembly mainAssembly = Assembly.GetExecutingAssembly();
+
+            string fullLocation = mainAssembly.Location;
+            string name = mainAssembly.ManifestModule.Name;
+
+            int location = fullLocation.IndexOf(name);
+            string directory = fullLocation.Substring(0, location);
+
+            string sewebar = directory + "Sewebar.dll";
+            string cook = directory + "CookComputing.XmlRpcV2.dll";
+
+            if (!File.Exists(sewebar) || !File.Exists(cook))
+            {
+                MessageBox.Show("The libraries Sewebar.dll or CookComputing.XmlRpcV2.dll do not exist in the directory with executable", 
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
             }
             else
             {
-                Application.Run(new SEWEBARForm());
+                return true;
             }
         }
     }
